f950f4e88068824294a0cfe7af1ab05abf135fcc
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2024-10-06 20:00:01 +0300

    feat: use content layer api

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
+	loader: glob({ pattern: '**/[^_]*.mdx', base: './src/data/blog' }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
+		heroImage: z.string().optional()
+	})
+})
 
-export const collections = { blog };
+export const collections = { blog }
