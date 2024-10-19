47d9016a9d4121e3cfe779affbe63981f4889607
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2024-10-19 12:59:35 +0300

    load plain markdown

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content'
 import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.mdx', base: './src/data/blog' }),
+	loader: glob({ pattern: '**/[^_]*.md?(x)', base: './src/data/blog' }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
