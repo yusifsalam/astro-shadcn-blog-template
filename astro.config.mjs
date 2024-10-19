b6f9b3a05877200daddf9053762ad5313d38cd50
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2024-10-19 14:59:06 +0300

    set site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://astro-shadcn-blog-template.vercel.app',
 	integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false }), react()],
 	prefetch: true
 })
